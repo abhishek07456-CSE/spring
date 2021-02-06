c7fd130d0d944cc1525ff133ffc895bf16db061b
abhishek07456 <abhishek07456@gmail.com>
2021-02-06 17:12:26 +0530

    Required Bean Annotaton Implemented

--- src/Controller/PersonController.java
+++ src/Controller/PersonController.java
@@ -3,19 +3,21 @@ package Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
 
 import Model.Skills;
 import Prototype.Person;
 
 public class PersonController {
-		
 	    private Person person; // or student and teacher;
-	    @Autowired(required=false)
+	    
+	    @Autowired
 		@Qualifier("student")
+	    @Required
+
 		public void setPerson(Person person) {
 			this.person = person;
 		}
-
 		public void personDetails() {
 			 if(this.person!=null) {
 				 System.out.println("\n----------------person details-------------------");
